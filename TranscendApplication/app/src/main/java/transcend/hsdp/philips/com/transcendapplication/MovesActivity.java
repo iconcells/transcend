9840efd05226313044ca34bde99ce76de7c245af
Anand Krishnamurthy <anand@cs.wisc.edu>
2015-03-08 10:12:26 -0700

    Moves APIs

--- TranscendApplication/app/src/main/java/transcend/hsdp/philips/com/transcendapplication/MovesActivity.java
+++ TranscendApplication/app/src/main/java/transcend/hsdp/philips/com/transcendapplication/MovesActivity.java
@@ -2,6 +2,7 @@ package transcend.hsdp.philips.com.transcendapplication;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
 
@@ -9,6 +10,7 @@ import com.philips.hsdp.adapters.ObservationListViewAdapter;
 import com.philips.hsdp.feed.LoginManager;
 import com.philips.hsdp.feed.ObservationFeed;
 import com.philips.hsdp.feed.OrganizationFeed;
+import com.philips.hsdp.model.HSDPObservation;
 import com.philips.hsdp.model.HSDPOrganization;
 
 import java.util.List;
@@ -19,64 +21,88 @@ import java.util.Observer;
  * Created by karthshe on 3/7/15.
  */
 public class MovesActivity extends Activity {
-    String mPatientUrlLink = null;
 
-    ObservationListViewAdapter mObservationAdapter;
+    public static String loginName = "mark.taylor", loginPwd = "Going4ther$";
+    //public static String loginName = "sam.s.smith", loginPwd = "MyFood4Health!";
 
-    TextView mObservationCount;
+    ObservationFeed stepsFeed = null;
+    private String mPatientUrlLink = null;
 
-    OrganizationFeed mOrganizationFeed = null;
-    ObservationFeed mObservationFeed = null;
+    private static String STEPS_STR = "https://rtmms.nist.gov|8454247";
+    //private static String ENERGY_STR = "https://rtmms.nist.gov|8454263";
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_moves);
 
-//        mPatientUrlLink = LoginManager.getInstance().getPatientUrlIdString(); // this.getIntent().getStringExtra(Constants.PATIENT_URL_LINK);
-//
-//        mOrganizationFeed = OrganizationFeed.getNewInstance();
-//        String organizationFeedUrl = OrganizationFeed.generateOrganizationFeedUrl (LoginManager.getInstance().getOrganizationUrlIdString());
-//        mOrganizationFeed.setFeedUrl (organizationFeedUrl);
-//        mOrganizationFeed.addObserver(new Observer()
-//        {
-//            @Override
-//            public void update(Observable observable, Object data)
-//            {
-//                if (mOrganizationFeed.getOrganizationList() != null)
-//                {
-//                    List<HSDPOrganization> orgList = mOrganizationFeed.getOrganizationList();
-//                    HSDPOrganization organization = orgList.get(0);
-////                    ((TextView) findViewById(R.id.patientOrganization)).setText(organization.getOrganizationName());
-//                }
-//            }
-//        });
-//        mOrganizationFeed.refresh();
-//
-//        mObservationFeed = ObservationFeed.getInstance();
-//        String observationFeedUrl = ObservationFeed.generateObservationUrl(mPatientUrlLink.replace("/Patient/", ""), null, null, null);
-//        //Log.i("HS", "ObservationFeedUrl: " + observationFeedUrl);
-//        mObservationFeed.setFeedUrl(observationFeedUrl);
-//        mObservationFeed.addObserver(new Observer() {
-//            @Override
-//            public void update(Observable observable, Object data)
-//            {
-//                refreshObservationView();
-//            }
-//        });
+        LoginManager loginManager = LoginManager.getInstance();
+        loginManager.addObserver (new Observer()
+        {
+            @Override
+            public void update(Observable observable, Object data)
+            {
+                handleLoginResponse();
+            }
+        });
 
+
+        stepsFeed = ObservationFeed.getNewInstance();
+
+        LoginManager.getInstance().authenticate(loginName, loginPwd, null);
 
     }
 
-    private void refreshObservationView() {
+    private void handleLoginResponse()
+    {
 
-        mObservationAdapter.notifyDataSetChanged();
-        int count = mObservationAdapter.getCount(); // mObservationFeed.getObservationList().size();
-        mObservationCount.setText("Page: " + mObservationFeed.getCurrentPageNo() + "/" + mObservationFeed.getTotalNoPages()
-                + " (" + mObservationFeed.getTotalRecordsOnServer() +" Records)");
+        if (LoginManager.getInstance().isAuthenticated())
+        {
+            Log.i("HS", "LOGGED IN");
 
-//            mNextPageObservationButton.setVisibility(mObservationFeed.getNextPageLinkUrl() != null ? View.VISIBLE : View.GONE);
-//            mPrevPageObservationButton.setVisibility(mObservationFeed.getPreviousPageLinkUrl() != null ? View.VISIBLE : View.GONE);
-//            findViewById(R.id.observationProgressBar).setVisibility(View.GONE);
+            mPatientUrlLink = LoginManager.getInstance().getPatientUrlIdString();
+            String before =null, after =null;
+
+            String inWhatObservations = STEPS_STR;
+            String glucoseObservationFeedUrl = ObservationFeed.generateObservationUrl(
+                    mPatientUrlLink.replace("/Patient/", ""), inWhatObservations, before, after);
+            Log.i("HS", "ObservationFeedUrl: " + glucoseObservationFeedUrl);
+            stepsFeed.setFeedUrl(glucoseObservationFeedUrl);
+            stepsFeed.addObserver(new Observer()
+            {
+                @Override
+                public void update(Observable observable, Object data)
+                {
+                    Log.i("HS", "Got observation data ");
+                    for(HSDPObservation observation : stepsFeed.getObservationList()) {
+                        Log.i("HS", observation.getObservationName() + " -- " + observation.getObservationValue() + "--" + observation.getAppliedDateTimeHumanReadableString());
+                        TextView text = (TextView) findViewById(R.id.stepsLabel);
+                        String [] sub = observation.getObservationValue().split("\\s");
+                        text.setText(sub[0]);
+                        double stepsDbl = Double.parseDouble(sub[0]);
+                        int steps = (int) stepsDbl;
+                        int distance = (int) ((double)steps * 0.4);
+                        TextView textDistance = (TextView) findViewById(R.id.dietLabel);
+                        textDistance.setText(String.valueOf(distance));
+                        break;
+                    }
+                }
+            });
+
+
+            pollCurrentObservations();
+        }
+        else
+        {
+            Log.i("HS", "NOT LOGGED IN");
+        }
+
     }
+
+    private void pollCurrentObservations() {
+        stepsFeed.refresh();
+    }
+
+
 }
