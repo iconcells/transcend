43d3189273a5a31e8a94a7f7baa5b4d65364a42d
Anand Krishnamurthy <anand@cs.wisc.edu>
2015-03-07 17:00:09 -0800

    Transcend Application

---
+++ TranscendApplication/app/src/androidTest/java/transcend/hsdp/philips/com/transcendapplication/ApplicationTest.java
@@ -0,0 +1,13 @@
+package transcend.hsdp.philips.com.transcendapplication;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class ApplicationTest extends ApplicationTestCase<Application> {
+    public ApplicationTest() {
+        super(Application.class);
+    }
+}
